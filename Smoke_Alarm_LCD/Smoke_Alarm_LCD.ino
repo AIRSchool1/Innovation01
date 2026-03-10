0ebc491b5d8eec79111381e7a7310f209abe2045
kridtapak <65170014@go.buu.ac.th>
2026-03-10 16:27:32 +0700

    Add files via upload

---
+++ Smoke_Alarm_LCD/Smoke_Alarm_LCD.ino
@@ -0,0 +1,61 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+int smokeSensor = A0;
+int buzzer = 8;
+
+int smokeValue;
+
+void setup()
+{
+  pinMode(smokeSensor, INPUT);
+  pinMode(buzzer, OUTPUT);
+
+  lcd.init();
+  lcd.backlight();
+
+  Serial.begin(9600);
+
+  lcd.setCursor(0,0);
+  lcd.print("Smoke Detector");
+  lcd.setCursor(0,1);
+  lcd.print("Warming Sensor");
+  
+  delay(20000);   // MQ sensor warm up
+  lcd.clear();
+}
+
+void loop()
+{
+  smokeValue = analogRead(smokeSensor);
+
+  Serial.print("Smoke Value: ");
+  Serial.println(smokeValue);
+
+  lcd.setCursor(0,0);
+  lcd.print("Smoke Level:   ");
+
+  lcd.setCursor(0,1);
+
+  if(smokeValue < 300)
+  {
+    lcd.print("NO FIRE       ");
+    noTone(buzzer);
+  }
+
+  else if(smokeValue >= 300 && smokeValue < 500)
+  {
+    lcd.print("FIRE WARNING  ");
+    tone(buzzer,1000);
+  }
+
+  else
+  {
+    lcd.print("FIRE ALERT!!! ");
+    tone(buzzer,1500);
+  }
+
+  delay(500);
+}
