186d1c03cb0c20c0c2b1d0ea37074ef26c126b0f
kridtapak <65170014@go.buu.ac.th>
2026-03-11 11:26:59 +0700

    Add files via upload

--- Water_Level/Water_Level.ino
+++ Water_Level/Water_Level.ino
@@ -1,39 +1,59 @@
-const int analogInPin = A0;
-int sensorValue = 0;
+int waterSensor = A0;
+
+int ledLow = 3;
+int ledMedium = 4;
+int ledHigh = 5;
+
+int buzzer = 8;
+
+int waterValue;
 
 void setup() {
-  // declare pin  to be an output:
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
+
+  pinMode(ledLow, OUTPUT);
+  pinMode(ledMedium, OUTPUT);
+  pinMode(ledHigh, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
   Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  sensorValue = analogRead(analogInPin);
-  Serial.print("sensor = ");
-  Serial.print(sensorValue);
-  Serial.print("\n");
-  delay(2);
-  if((sensorValue>=100)&&(sensorValue<=600)){
-    digitalWrite(2,HIGH);
-    delay(100);
-    }
-  else if((sensorValue>=601)&&(sensorValue<=625)){
-   digitalWrite(3,HIGH);
-   delay(100);
-    }  
-  else if((sensorValue>=626)&&(sensorValue<=700)){
-    digitalWrite(4,HIGH);
-    digitalWrite(5,HIGH);
-    }
-  else{
-    digitalWrite(2,LOW);
-    digitalWrite(3,LOW);
-    digitalWrite(4,LOW);
-    digitalWrite(5,LOW);
-    delay(100);
-    }
+
+  waterValue = analogRead(waterSensor);
+
+  Serial.print("Water Level: ");
+  Serial.println(waterValue);
+
+  // น้ำต่ำ
+  if (waterValue < 300) {
+
+    digitalWrite(ledLow, HIGH);
+    digitalWrite(ledMedium, LOW);
+    digitalWrite(ledHigh, LOW);
+
+    noTone(buzzer);
+  }
+
+  // น้ำระดับกลาง
+  else if (waterValue >= 300 && waterValue < 600) {
+
+    digitalWrite(ledLow, LOW);
+    digitalWrite(ledMedium, HIGH);
+    digitalWrite(ledHigh, LOW);
+
+    tone(buzzer, 1000);
+  }
+
+  // น้ำสูง
+  else {
+
+    digitalWrite(ledLow, LOW);
+    digitalWrite(ledMedium, LOW);
+    digitalWrite(ledHigh, HIGH);
+
+    tone(buzzer, 1500);
+  }
+
+  delay(500);
 }
