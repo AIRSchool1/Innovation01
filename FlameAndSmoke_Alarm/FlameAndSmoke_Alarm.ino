281ac18425408dc7f6373455dcbd03812a186ef9
kridtapak <65170014@go.buu.ac.th>
2026-03-13 12:08:41 +0700

    Add files via upload

--- FlameAndSmoke_Alarm/FlameAndSmoke_Alarm.ino
+++ FlameAndSmoke_Alarm/FlameAndSmoke_Alarm.ino
@@ -38,40 +38,42 @@ void loop()
   Serial.print("  Flame: ");
   Serial.println(flameValue);
 
-  // แสดงค่า Smoke
-  lcd.setCursor(0,0);
-  lcd.print("S:");
-  lcd.print(smokeValue);
-  lcd.print("   ");
+  bool smokeDetected = smokeValue >= 300;
+  bool flameDetected = flameValue <= 500;
 
-  // แสดงค่า Flame
-  lcd.setCursor(0,1);
-  lcd.print("F:");
-  lcd.print(flameValue);
-  lcd.print("   ");
+  lcd.clear();
 
-  // ตรวจจับเหตุจริงเท่านั้น
-  if(smokeValue > 500) 
+  if(smokeDetected && flameDetected)
   {
-    lcd.setCursor(10,0);
-    lcd.print("SMOKE");
-    tone(buzzer,1200);
+    lcd.setCursor(0,0);
+    lcd.print("WARNING!");
+    lcd.setCursor(0,1);
+    lcd.print("FIRE & SMOKE!");
+    tone(buzzer,1000);
   }
-
-  else if(flameValue < 400)
+  else if(smokeDetected)
   {
-    lcd.setCursor(10,1);
-    lcd.print("FIRE ");
-    tone(buzzer,1500);
+    lcd.setCursor(0,0);
+    lcd.print("WARNING!");
+    lcd.setCursor(0,1);
+    lcd.print("SMOKE DETECTED");
+    tone(buzzer,1000);
   }
-
+  else if(flameDetected)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("WARNING!");
+    lcd.setCursor(0,1);
+    lcd.print("FIRE DETECTED!");
+    tone(buzzer,1000);
+  }
   else
   {
+    lcd.setCursor(0,0);
+    lcd.print("NOW IS SAFE");
+    lcd.setCursor(0,1);
+    lcd.print("No Fire/Smoke");
     noTone(buzzer);
-    lcd.setCursor(10,0);
-    lcd.print("     ");
-    lcd.setCursor(10,1);
-    lcd.print("     ");
   }
 
   delay(500);
